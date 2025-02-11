0b417cf44a2523210f5024d21814c3da78e10b78
Joao <joaogalbier@gmail.com>
2025-02-11 16:04:10 -0500

    Settings & YAML files, ready for deployment

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -27,6 +27,31 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Platform.sh settings.
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / 'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials('database')
+        DATABASES = {
+            'default' : {
+                'ENGINE' : 'django.db.backends.postresql',
+                'NAME' : db_settings['path'],
+                'USER' : db_settings['username'],
+                'PASSWORD' : db_settings['password'],
+                'HOST' : db_settings['host'],
+                'PORT' : db_settings['port'],
+            },
+        }
+
 # My settings.
 LOGIN_REDIRECT_URL = 'learning_logs:topics'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
