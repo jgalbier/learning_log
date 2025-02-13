dab45c035306c79daec066a06a84061493b3948a
Joao <joaogalbier@gmail.com>
2025-02-13 11:01:26 -0500

    fixed overriding database settings with defaults

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -27,6 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 # Platform.sh settings.
 from platformshconfig import Config
 import os
@@ -114,17 +125,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'll_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
